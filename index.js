c5bb78b1f18efae070653e26d20da7051fa20e11
hasemo1999 <hasemo1999@gmail.com>
2025-06-24 21:29:00 +0900

    index.js を更新

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const line = require('@line/bot-sdk');
+import express from 'express';
+import { middleware, Client } from '@line/bot-sdk';
 
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
@@ -7,23 +7,25 @@ const config = {
 };
 
 const app = express();
-app.use(line.middleware(config));
+app.use(middleware(config));
 
-const client = new line.Client(config);
+const client = new Client(config);
 
 app.post('/webhook', (req, res) => {
   Promise
-    .all(req.body.events.map(event => {
-      return client.replyMessage(event.replyToken, {
-        type: 'text',
-        text: 'こんにちは！'
-      });
-    }))
-    .then(() => res.end())
-    .catch((err) => {
-      console.error(err);
-      res.status(500).end();
-    });
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
 });
+
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: `「${event.message.text}」って言ったね！`, // ここは好きに変えてOK
+  });
+}
 
 app.listen(3000);
